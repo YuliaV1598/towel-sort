4b7c90df9f8046668c0171881d14f1989e88059f
Yulia <Yulia159V@gmail.com>
2021-03-15 13:34:03 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 module.exports = function towelSort (matrix) {
 	let res = [];
+	if (matrix == null){
+		return res;
+	}
 	for (let i = 1; i < matrix.length; i = i+2) {
 		let arr1 = matrix[i].sort(function(a,b){return b - a});
 	}
